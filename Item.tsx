e962eee46357409dc99a2b332b0f43759438bb51
Julian Noziglia <jnoziglia@codicilabs.com>
2023-07-07 17:31:37 -0300

    Adding amount to list and buttons to edit it

--- Item.tsx
+++ Item.tsx
@@ -17,7 +17,22 @@ function Item({ id, name, amount }) {
     <>
       <List.Item
         title={name}
+        right={props => <Amount {...props} id={id} name={name} amount={amount} />}
       />
+    </>
+  );
+}
+
+function Amount({ id, name, amount }) {
+  return (
+    <>
+      <Button mode="elevated" onPress={() => console.log('Pressed')}>
+        -
+      </Button>
+      <Text style={{textAlignVertical: 'center'}}>{amount}</Text>
+      <Button mode="elevated" onPress={() => console.log('Pressed')}>
+        +
+      </Button>
     </>
   );
 }
